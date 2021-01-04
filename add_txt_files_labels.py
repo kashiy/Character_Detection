df6be036521b398c665ff08c2329606b368a6c0c
Yuval Kashi <kashiy@post.bgu.ac.il>
2021-01-04 23:55:37 +0200

    take txt and copy them from src to dst with dark and bright .txt

---
+++ add_txt_files_labels.py
@@ -0,0 +1,29 @@
+#Import Necessary Libraries
+import glob
+import os
+import shutil
+
+# store new images in your dir you want
+write_to_directory = r"C:\Users\Yuval Kashi\Downloads\morty_standing_bright_dark_jpeg_NO_LABEL"
+if not os.path.exists(write_to_directory):
+    os.makedirs(write_to_directory)
+
+# your path to images folder source
+source_dir = r"C:\Users\Yuval Kashi\Downloads\morty_standing_WITH_LABEL_JPEG"
+for img in glob.glob(source_dir+"\*.txt"):
+    print(img)
+    fileName_absolute = os.path.basename(img)  ## Now get the file name with os.path.basename
+
+    #.txt
+    txt = fileName_absolute[:-4]
+
+    new_filename_dark = write_to_directory+"\\"+txt+" dark.txt"
+    new_filename_bright = write_to_directory+ "\\"+txt+ " bright.txt"
+
+    try:
+        shutil.copyfile(img ,new_filename_dark )
+        print(new_filename_dark)
+        shutil.copyfile(img ,new_filename_bright )
+        print(new_filename_bright)
+    except OSError as e:
+        print("Something happened:", e)
